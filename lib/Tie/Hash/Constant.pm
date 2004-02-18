335eaeb2a00927a5772d16eb72379dd4866680df
richardc <richardc@c56e198b-7db7-0310-b3ab-8e406f684e33>
2004-02-18 19:35:09 +0000

    mmm rocks
    git-svn-id: file:///web/svn.unixbeard.net/repo/richardc/perl/Tie-Hash-Constant@2145 c56e198b-7db7-0310-b3ab-8e406f684e33

---
+++ lib/Tie/Hash/Constant.pm
@@ -0,0 +1,49 @@
+package Tie::Hash::Constant;
+
+use strict;
+#use warnings;
+
+
+
+=head1 NAME
+
+Tie::Hash::Constant - 
+
+=head1 SYNOPSIS
+
+  use Tie::Hash::Constant;
+
+
+
+=head1 DESCRIPTION
+
+
+
+=cut
+
+
+
+=head1 AUTHOR
+
+Written by Mark Fowler E<lt>mark@twoshortplanks.comE<gt>
+
+Copyright Mark Fowler 2004.  All Rights Reserved.
+
+This program is free software; you can redistribute it
+and/or modify it under the same terms as Perl itself.
+
+
+=head1 BUGS
+
+None known.
+
+Bugs should be reported to me via the CPAN RT system.
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Tie::Hash::Constant>.
+
+
+=head1 SEE ALSO
+
+
+=cut
+
+1;
